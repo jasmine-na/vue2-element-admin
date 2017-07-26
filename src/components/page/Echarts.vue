8ddfad29d1bda81cb48ba94189b9fc65a3e92124
unknown <liyne@ufsoft.com.cn>
2017-07-26 15:20:27 +0800

    add echart

--- src/components/page/Echarts.vue
+++ src/components/page/Echarts.vue
@@ -5,6 +5,7 @@
                 <div  id="echarts2" class="echart"></div>
                 <div  id="Dashboard" class="echart"></div>
                 <div  id="echarts3" class="echart"></div>
+                <div  id="echarts4" class="echart"></div>
      </div>
 </template>
 <!-- <script type="text/ecmascript-6"> -->
@@ -21,6 +22,7 @@ export default {
         this.dashboard();
         this.echarts2();
         this.echarts3();
+        this.echarts4();
     },
     methods: {
              dashboard() {
@@ -273,6 +275,66 @@ export default {
                                     shadowColor: 'rgba(0, 0, 0, 0.5)'
                                 }
                             }
+                        }
+                    ]
+                };
+
+
+
+                // 使用刚指定的配置项和数据显示图表。
+                myChart.setOption(option);
+            },
+            echarts4() {
+                    // 基于准备好的dom，初始化echarts实例
+                var myChart = echarts.init(document.getElementById('echarts4'));
+
+                // 指定图表的配置项和数据
+                var option = {
+                    title: {
+                        text: 'Step Line'
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data:['Step Start', 'Step Middle', 'Step End']
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    toolbox: {
+                        feature: {
+                            saveAsImage: {}
+                        }
+                    },
+                    xAxis: {
+                        type: 'category',
+                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                    },
+                    yAxis: {
+                        type: 'value'
+                    },
+                    series: [
+                        {
+                            name:'Step Start',
+                            type:'line',
+                            step: 'start',
+                            data:[120, 132, 101, 134, 90, 230, 210]
+                        },
+                        {
+                            name:'Step Middle',
+                            type:'line',
+                            step: 'middle',
+                            data:[220, 282, 201, 234, 290, 430, 410]
+                        },
+                        {
+                            name:'Step End',
+                            type:'line',
+                            step: 'end',
+                            data:[450, 432, 401, 454, 590, 530, 510]
                         }
                     ]
                 };
